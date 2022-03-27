2197cecd7e302586239c7ad6ee9ec949985d435c
Raul (lok0zzz) <49105981+lok0zzz@users.noreply.github.com>
2022-03-27 11:37:10 -0300

    Delete dados.py

--- dados.py
+++
@@ -1,6 +0,0 @@
-dados = {
-    'consumer_key': 'uye5F9xIG5Rr3d20Ik0McQvfF',
-    'consumer_secret': 'MFRU91G3RBUDU4h5dSLiQu2oUyhGId0geBIhasiE4dct92fqvx',
-    'access_token': '1507106036938850311-IPpOpU53wqw8t9V1NkUez1lQJFd0At',
-    'access_token_secret': 'wTlgegXbrnWaWx4FTo3c5NK5wRfqsHAovMXnnj9E2gUdm'
-}
