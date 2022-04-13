8490bbfdb1acfff576df0021cd38a2b591d5c547
Raul (lok0zzz) <49105981+lok0zzz@users.noreply.github.com>
2022-04-13 19:40:33 -0300

    Twitter Bot v1.7
    Refatoração do código, arrumando visualização das mensagens do console e do Tweet.

--- client.py
+++ client.py
@@ -5,19 +5,21 @@ import requests
 import time
 import dados as d            # Local onde está armazenado meus tokens cujos quais não irei divulgar publicamente.
 
+# Código criado por LOKOZ
 
 # Autenticando o Bot.
 client = tw.Client(
-    consumer_key=d.dados['consumer_key'], consumer_secret=d.dados['consumer_secret'], access_token=d.dados['access_token'], access_token_secret=d.dados['access_token_secret']
+    consumer_key=d.consumer_key,
+    consumer_secret=d.consumer_secret,
+    access_token=d.access_token,
+    access_token_secret=d.access_token_secret
 )
 
 # Fazendo uma requisição inicial.
 try:
     req_inicial = requests.get('http://economia.awesomeapi.com.br/json/last/USD-BRL')
-
     req_inicial_dic = req_inicial.json()
-
-    cot_dolar_inicio = req_inicial_dic['USD']['bid']
+    cot_dolar_inicio = req_inicial_dic['USDBRL']['bid']
 
 except Exception as e:
     print('[ERROR] Ocorreu algum erro na requisição da API.')
@@ -27,16 +29,13 @@ except Exception as e:
 while True:
     i = 0
     while i < 1:
-        print(datetime.now().strftime('%d/%m/%Y %H:%M:%S - Waiting Cooldown'))
         time.sleep(60)
         i = i + 1
 
     try:
         requisicao = requests.get('http://economia.awesomeapi.com.br/json/last/USD-BRL')
-
         requisicao_dic = requisicao.json()
-
-        cot_dolar = requisicao_dic['USD']['bid']
+        cot_dolar = requisicao_dic['USDBRL']['bid']
 
     except TimeoutError:
         print(datetime.now().strftime('%d/%m/%Y %H:%M:%S - Timeout Error'))
@@ -44,21 +43,23 @@ while True:
         print(datetime.now().strftime('%d/%m/%Y %H:%M:%S - ERROR'))
         print(e)
 
-
     # Formatando os números.
-    cot_dolar_inicio = float(cot_dolar_inicio)
-    cot_dolar = float(cot_dolar)
+    try:
+        cot_dolar_inicio = float(cot_dolar_inicio)
+        cot_dolar = float(cot_dolar)
+
+    except NameError as E:
+        datetime.now().strftime('%d/%m/%Y %H:%M:%S - ERROR')
+
+    except Exception as E:
+        print(datetime.now().strftime('%d/%m/%Y %H:%M:%S - Error'))
 
     cot_dolar_inicio = "{:.2f}".format(cot_dolar_inicio)
     cot_dolar = '{:.2f}'.format(cot_dolar)
 
-    print(f'COTAÇÃO ANTERIOR: {cot_dolar_inicio}')
-    print(f'COTAÇÃO ATUAL: {cot_dolar}\n\n')
-
     # Calculando variação.
     variacao_descida = float(cot_dolar_inicio) - float(cot_dolar)
     variacao_subida = float(cot_dolar) - float(cot_dolar_inicio)
-
 
     # Pegar data e hora atuais.
     data_e_hora_atuais = datetime.now()
@@ -68,17 +69,16 @@ while True:
         response = client.create_tweet(
             text=f'''
 
-[{data_e_hora_atuais.strftime('%d/%m/%Y %H:%M')}]
+• [{data_e_hora_atuais.strftime('%d/%m/%Y %H:%M')}]
 
-📉 Dólar desceu.
 
-💵 R${cot_dolar} (-{round(variacao_descida, 2)})
+📉 Dolár desceu
 
-        '''
+💵 Foi para: R${cot_dolar} 
+🤓 Variação de: R$-{round(variacao_descida, 2)}
+                  '''
         )
-
         print(f"https://twitter.com/user/status/{response.data['id']}")
-
         cot_dolar_inicio = cot_dolar
 
     elif cot_dolar_inicio < cot_dolar:
@@ -86,20 +86,16 @@ while True:
         response = client.create_tweet(
             text=f'''
 
-[{data_e_hora_atuais.strftime('%d/%m/%Y %H:%M')}]
+• [{data_e_hora_atuais.strftime('%d/%m/%Y %H:%M')}]
 
-📉 Dólar subiu.
 
-💵 R${cot_dolar} (+{round(variacao_subida, 2)})
+💵 Foi para: R${cot_dolar} 
+🤓 Variação de: R$-{round(variacao_subida, 2)}
 
-                '''
+                  '''
         )
-
         print(f"https://twitter.com/user/status/{response.data['id']}")
-
         cot_dolar_inicio = cot_dolar
 
     else:
         pass
-
-    # CREATED BY LOKOZ
